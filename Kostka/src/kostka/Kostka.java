0309e8ee1d21ee4457bd457a1f938c20b15381c9
Adam Małota <adammalota999@interia.eu>
2018-10-17 14:39:15 +0200

    17.10.18

--- Kostka/src/kostka/Kostka.java
+++ Kostka/src/kostka/Kostka.java
@@ -1,40 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project
-Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kostka;
 
 import java.util.Random;
 import java.util.Scanner;
 
 /**
- *
- * @author Student
+ * @author Adam
  */
 public class Kostka {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Random r = new Random();
-        //Scanner in = new Scanner(System.in);
+
         Player player = new Player();
         int number, random;
 
+        player.setName("Adam");
+
         do {
-            System.out.print("Wpisz liczbe 1-6\n");
+            System.out.print(player.getName() + " Wpisz liczbe 1-6\n");
             number = player.guess();
             random = r.nextInt(6) + 1;
             if (number != random) {
-                System.out.print("Źle. Podałeś " + number +
-" a na kostce było " + random + "\n");
+                System.out.print("Źle. " + player.getName() + " podałeś " + number + " a na kostce było " + random + "\n");
             }
         } while (number != random);
 
-        System.out.print("Wpisałeś " + number + " i zgadłeś\n");
+        System.out.print(player.getName() + " wpisałeś " + number + " i zgadłeś\n");
 
     }
 
